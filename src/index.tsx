7cce532d4d9e9a945533b0aae9d25ff4b523c89b
hello-world-OHNO <daruma0248@gmail.com>
2024-02-13 13:30:38 +0900

    修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import Form from './Form.tsx';
+import Form from './Form';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Form />
-  </React.StrictMode>
-);
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Form />
+    </React.StrictMode>,
+    root
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
